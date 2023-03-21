02f2344d542b616d5e18ad905937b02720291d5e
B. Cem Sayilar <96774646+cemsayilar@users.noreply.github.com>
2023-03-21 18:50:12 +0300

    Create sql_case.sql

---
+++ sql_case.sql
@@ -0,0 +1,6 @@
+/*There are four types of joins in SQL:
+
+/*INNER JOIN: returns only the matching rows from both tables
+/*LEFT JOIN: returns all rows from the left table and the matching rows from the right table
+/*RIGHT JOIN: returns all rows from the right table and the matching rows from the left table
+/*FULL OUTER JOIN: returns all rows from both tables, with NULL values for non-matching rows.
